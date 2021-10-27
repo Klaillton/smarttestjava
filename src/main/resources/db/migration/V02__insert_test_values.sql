4d7e8cc4667a5f7a1df8f00403118068a7a6836a
Klaillton <klaillton@gmail.com>
2021-10-27 21:44:54 +0100

    004-Melhorias

--- src/main/resources/db/migration/V02__insert_test_values.sql
+++ src/main/resources/db/migration/V02__insert_test_values.sql
@@ -1,3 +1,3 @@
-INSERT INTO ocurrences (datatime, number) VALUES (current_timestamp, '5');
-INSERT INTO ocurrences (datatime, number) VALUES (current_timestamp, '14');
-INSERT INTO ocurrences (datatime, number) VALUES (current_timestamp, '25');
+INSERT INTO occurrences (datetime, number) VALUES (current_timestamp, '5');
+INSERT INTO occurrences (datetime, number) VALUES (current_timestamp, '14');
+INSERT INTO occurrences (datetime, number) VALUES (current_timestamp, '25');
